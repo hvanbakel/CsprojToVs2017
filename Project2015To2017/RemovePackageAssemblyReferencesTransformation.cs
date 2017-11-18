8d4e05f453b8557f0b89396594339bbfab68473a
Hans van Bakel <jwvanbakel@gmail.com>
2017-11-17 20:09:38 -0800

    Fixes #30

---
+++ Project2015To2017/RemovePackageAssemblyReferencesTransformation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Project2015To2017.Definition;
+
+namespace Project2015To2017
+{
+	internal sealed class RemovePackageAssemblyReferencesTransformation : ITransformation
+	{
+		public Task TransformAsync(XDocument projectFile, DirectoryInfo projectFolder, Project definition)
+		{
+			if (definition.PackageReferences.Count == 0)
+			{
+				return Task.CompletedTask;
+			}
+
+			var packageReferenceIds = definition.PackageReferences.Select(x => x.Id).ToArray();
+			definition.AssemblyReferences.RemoveAll(x => 
+				x.HintPath != null && 
+				packageReferenceIds.Any(p => x.HintPath.IndexOf(@"packages\" + p, StringComparison.OrdinalIgnoreCase) > 0));
+
+			return Task.CompletedTask;
+		}
+	}
+}
---
+++ Project2015To2017Tests/RemovePackageAssemblyReferencesTransformationTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project2015To2017;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project2015To2017Tests
+{
+	[TestClass]
+    public class RemovePackageAssemblyReferencesTransformationTest
+    {
+		[TestMethod]
+		public void DedupeReferencesFromPackages()
+		{
+			var project = new Project2015To2017.Definition.Project
+			{
+				AssemblyReferences = new List<Project2015To2017.Definition.AssemblyReference>
+				{
+					new Project2015To2017.Definition.AssemblyReference
+					{
+						Include = "Newtonsoft.Json, Version=10.0.0.0, Culture=neutral, PublicKeyToken=30ad4fe6b2a6aeed, processorArchitecture=MSIL",
+						HintPath = @"..\packages\Newtonsoft.Json.10.0.2\lib\net45\Newtonsoft.Json.dll"
+					},
+					new Project2015To2017.Definition.AssemblyReference
+					{
+						Include = "System.Data.DataSetExtensions"
+					},
+					new Project2015To2017.Definition.AssemblyReference
+					{
+						Include = "Owin",
+						HintPath = @"..\packages\Owin.1.0\lib\net40\Owin.dll"
+					}
+				},
+				PackageReferences = new[]
+				{
+					new Project2015To2017.Definition.PackageReference
+					{
+						Id = "Newtonsoft.Json"
+					}
+				}
+			};
+
+			var transformation = new RemovePackageAssemblyReferencesTransformation();
+			transformation.TransformAsync(null, null, project);
+
+			Assert.AreEqual(2, project.AssemblyReferences.Count);
+		}
+    }
+}
