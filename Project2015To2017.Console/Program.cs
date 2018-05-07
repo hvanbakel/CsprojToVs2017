7938fb8944f9875aa8071e3f4c37d5e8c6436fc3
Mark Adamson <mark@adamsonfamily.co.uk>
2018-05-07 17:04:57 +0100

    Add --dry-run option
    Using '--dry-run' as it is what apt-get uses
    
    Fixes #92

--- Project2015To2017.Console/Program.cs
+++ Project2015To2017.Console/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace Project2015To2017.Console
 {
@@ -8,20 +9,22 @@ namespace Project2015To2017.Console
         {
 	        if (args.Length == 0)
 	        {
-		        System.Console.WriteLine($"Please specify a project file.");
+		        System.Console.WriteLine("Please specify a project file.");
 		        return;
 	        }
 
 			var writer = new Project2015To2017.Writing.ProjectWriter();
-			foreach (var definition in ProjectConverter.Convert(args[0], new Progress<string>(System.Console.WriteLine)))
-			{
-				if (definition == null)
-				{
-					continue;
-				}
 
-				writer.Write(definition);
-			}
+	        var convertedProjects = ProjectConverter.Convert(args[0], new Progress<string>(System.Console.WriteLine))
+													.Where(x => x != null);
+
+	        if (!args.Contains("--dry-run"))
+	        {
+		        foreach (var project in convertedProjects)
+		        {
+			        writer.Write(project);
+		        }
+	        }
         }
     }
 }
