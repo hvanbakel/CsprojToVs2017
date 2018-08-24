ea727fc73305dd1dfdf547d5d54db5b63a98a58a
Mark Adamson <mark@adamsonfamily.co.uk>
2018-08-24 08:01:44 +0200

    Extract deletion method

--- Project2015To2017/Transforms/AssemblyAttributeTransformation.cs
+++ Project2015To2017/Transforms/AssemblyAttributeTransformation.cs
@@ -47,6 +47,11 @@ namespace Project2015To2017.Transforms
 				definition.AssemblyAttributeProperties = new[] { new XElement("GenerateAssemblyInfo", "false") };
 			}
 
+			MarkForDeletion(definition);
+		}
+
+		private void MarkForDeletion(Project definition)
+		{
 			if (definition.AssemblyAttributes.File != null && PointlessAssemblyInfo(definition.AssemblyAttributes))
 			{
 				definition.Deletions = definition
