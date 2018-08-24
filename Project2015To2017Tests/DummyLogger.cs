30ca404ec49e6836944e0217fb3a670f138d2c27
Mark Adamson <mark@adamsonfamily.co.uk>
2018-08-24 12:50:27 +0200

    Allow LogLevel to be changed in DummyLogger

--- Project2015To2017Tests/DummyLogger.cs
+++ Project2015To2017Tests/DummyLogger.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 
@@ -9,6 +9,8 @@ namespace Project2015To2017Tests
 		private readonly List<string> logs = new List<string>();
 		public IReadOnlyList<string> LogEntries => this.logs;
 
+		public LogLevel MinimumLogLevel { get; set; } = LogLevel.Error;
+
 		public IDisposable BeginScope<TState>(TState state)
 		{
 			throw new NotImplementedException();
@@ -16,12 +18,15 @@ namespace Project2015To2017Tests
 
 		public bool IsEnabled(LogLevel logLevel)
 		{
-			return logLevel == LogLevel.Error;
+			return logLevel >= MinimumLogLevel;
 		}
 
 		public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
 		{
-			this.logs.Add(formatter(state, exception));
+			if (IsEnabled(logLevel))
+			{
+				this.logs.Add(formatter(state, exception));
+			}
 		}
 	}
 }
