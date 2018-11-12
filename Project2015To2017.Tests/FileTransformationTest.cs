76c48c5b63a1e77a0bba0b31a43b273ad7271a55
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2018-11-12 19:43:48 +0200

    Add Antlr4 tag support

--- Project2015To2017.Tests/FileTransformationTest.cs
+++ Project2015To2017.Tests/FileTransformationTest.cs
@@ -47,10 +47,11 @@ namespace Project2015To2017.Tests
 
 			var includeItems = project.ItemGroups.SelectMany(x => x.Elements()).ToImmutableList();
 
-			Assert.AreEqual(29, includeItems.Count);
+			Assert.AreEqual(31, includeItems.Count);
 
 			Assert.AreEqual(12, includeItems.Count(x => x.Name.LocalName == "Reference"));
 			Assert.AreEqual(2, includeItems.Count(x => x.Name.LocalName == "ProjectReference"));
+			Assert.AreEqual(2, includeItems.Count(x=> x.Name.LocalName.Equals("Antlr4")));
 			Assert.AreEqual(11, includeItems.Count(x => x.Name.LocalName == "Compile"));
 			Assert.AreEqual(5, includeItems.Count(x => x.Name.LocalName == "Compile" && x.Attribute("Update") != null));
 			Assert.AreEqual(4, includeItems.Count(x => x.Name.LocalName == "Compile" && x.Attribute("Include") != null));
